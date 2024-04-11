8b97a57d7b4fc93232a040e5466565a06a0db2f1
“emitdlr” <emit@utexas.edu>
2024-04-10 20:32:59 -0500

    adding static files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css"; // Import CSS file for styling
+
+function App() {
+  return (
+    <div className="App">
+      <div className="background-logo"></div> {/* Background logo */}
+      <header className="App-header">
+        <h1>Globebug is coming soon!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
